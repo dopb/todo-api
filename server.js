5d4883bd758ef35efe59f3d9206b2390adbebb22
Diego Paredes Bassi <diegoparedesb@gmail.com>
2015-11-30 00:25:49 -0300

    Add POST for todos

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Do laundry',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('To-Do API Root');
@@ -38,6 +35,39 @@ app.get('/todos/:id', function (req, res) {
     };
 });
 
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    // add id field
+    body.id = todoNextId++;
+
+    // push body into array
+    todos.push(body);
+    res.json(body);
+});
+
 app.listen(PORT, function () {
     console.log('Express listening on port: ' + PORT);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
